286dbb6658f0d8abb042327ba3307c0661d44564
GURMAN1912 <manansinghsambhi@gmail.com>
2024-09-22 04:06:16 +0530

    Updating navbar and adding Hero banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar'
+// App.js
+import React from "react";
+import HeroBanner from "./components/hero/HeroBanner";
+import Navbar from "./components/navbar/Navbar";
+import bg from "./assets/bg.png"; // Import the background image
+
+// Define the bgStyles variable
+const bgStyles = {
+  backgroundImage: `url(${bg})`, // Set the background image using the imported bg
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  opacity: "0.2", // Set the opacity only for the background image
+};
 
 function App() {
-
   return (
     <>
-    <Navbar/>
+      {/* Background Image Div */}
+      <div
+        className="absolute inset-0 -z-10 bg-black"
+        style={bgStyles}
+      />
+
+      {/* Content */}
+      <Navbar />
+      <HeroBanner />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
