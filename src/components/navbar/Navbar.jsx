5c238d750c4726dd1807fc30196139190dba69dd
Dhanraj Singh <sdhanraj300@gmail.com>
2024-09-22 00:59:13 +0530

    added some more changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="h-20 fixed w-full bg-[#2ecc71] rounded-lg   bg-transparent shadow-md">
-      <p className="text-white font-bold text-3xl">hello</p>
+      <p className="text-white font-bold text-3xl">iPhone</p>
     </div>
   );
 };
