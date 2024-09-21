6e8865a022b55cab143dab8ef0d57dd07dcd42ab
kmTyagi198 <k.m.tyagi198@gmail.com>
2024-09-22 03:43:40 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import Testimony from './components/testimony/Testimony'
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Testimony/>
+    <Footer/>
     </>
   )
 }
